59498f6e6c215e8833b9277dbdeb7b3c8bfd7f81
Tadeo_Martinez <tadeobateria2006@gmai.com>
2025-09-22 08:30:44 -0600

    feat:resta

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ print(f"IP Address: {IPadd}")
 for i in range(0,10):
     print(f"Count: {i}")
 
-numero_a = input("Dame el primer número -> ")
-numero_b = input("Dame el segundo número -> ")
-print(f"La suma del número a más el número b es: {numero_a + numero_b}")
+numero_a = int(input("Dame el primer número -> "))
+numero_b = int(input("Dame el segundo número -> "))
+print(f"La suma del número a más el número b es: {numero_a + numero_b} y la resta de los números es: {numero_a - numero_b}")
