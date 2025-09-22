9dbfcef5e4737429da9251ba284cf732e585f617
Tadeo_Martinez <tadeobateria2006@gmai.com>
2025-09-22 08:29:09 -0600

    feat:suma

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ print(f"IP Address: {IPadd}")
 
 for i in range(0,10):
     print(f"Count: {i}")
+
+numero_a = input("Dame el primer número -> ")
+numero_b = input("Dame el segundo número -> ")
+print(f"La suma del número a más el número b es: {numero_a + numero_b}")
