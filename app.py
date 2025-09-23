0b82a724e9d664a427e3b3ab2945c3e164f94bba
Tadeo_Martinez <tadeobateria2006@gmai.com>
2025-09-23 08:34:57 -0600

    feat:multi

--- app.py
+++ app.py
@@ -12,3 +12,5 @@ for i in range(0,10):
 numero_a = int(input("Dame el primer número -> "))
 numero_b = int(input("Dame el segundo número -> "))
 print(f"La suma del número a más el número b es: {numero_a + numero_b} y la resta de los números es: {numero_a - numero_b}")
+
+print(numero_a * numero_b)
